d69866a9b10371155af70e2b2fd87b6ef3942fc1
Frances Leigh <fleighbaay@gmail.com>
2023-04-07 14:23:06 +0800

    First commit

---
+++ Baay, Frances Leigh G. - OOP A2P2.py
@@ -0,0 +1,18 @@
+#Use dictionary to define the subtitutes
+
+char_substitute = {'*': 'a', '&': 'e', '#': 'i', '+': 'o', '!': 'u'}
+
+#Prompt the user to input a text
+encrypted_text = input("\033[3mEnter an encrypted text to decrypt:\033[0m ")
+
+decrypted_text = ""
+
+#Check if a character from the dictionary is present in the encrypted_text
+#If character is present, it will retrieve the value and will be appended to decrypted text
+for char in encrypted_text:
+    if char in char_substitute:
+        decrypted_text += char_substitute[char]
+    else:
+        decrypted_text += char
+
+print("\033[1;33mDecrypted Text: \033[32m{}\033[1m".format(decrypted_text))
